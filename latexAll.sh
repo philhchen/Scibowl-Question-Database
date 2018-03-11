a655d03a26a028cd8cd89b7368e82f47cfe089eb
Phil <philchen2000@gmail.com>
2018-03-10 22:33:43 -0800

    Added subj option as second argument to latexMult.sh and latexShort.sh

--- latexAll.sh
+++ latexAll.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-./latexMult.sh b_m
-./latexMult.sh c_m
-./latexMult.sh en_m
-./latexMult.sh es_m
-./latexMult.sh m_m
-./latexMult.sh p_m
-./latexShort.sh b_s
-./latexShort.sh c_s
-./latexShort.sh en_s
-./latexShort.sh es_s
-./latexShort.sh m_s
-./latexShort.sh p_s
+./latexMult.sh b_m Biology
+./latexMult.sh c_m Chemistry
+./latexMult.sh en_m Energy
+./latexMult.sh es_m Earth and Space
+./latexMult.sh m_m Math
+./latexMult.sh p_m Physics
+./latexShort.sh b_s Biology
+./latexShort.sh c_s Chemistry
+./latexShort.sh en_s Energy
+./latexShort.sh es_s Earth and Space
+./latexShort.sh m_s Math
+./latexShort.sh p_s Physics
