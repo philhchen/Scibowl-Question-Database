654a0db2c7d57850151d32af385341d3d480e712
Phil <philchen2000@gmail.com>
2018-03-10 22:04:02 -0800

    Added latexAll and modified latexMult

---
+++ latexAll.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+./latexMult.sh b_m
+./latexMult.sh c_m
+./latexMult.sh en_m
+./latexMult.sh es_m
+./latexMult.sh m_m
+./latexMult.sh p_m
+./latexShort.sh b_m
+./latexShort.sh c_m
+./latexShort.sh en_m
+./latexShort.sh es_m
+./latexShort.sh m_m
+./latexShort.sh p_m
--- latexMult.sh
+++ latexMult.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 awk 'BEGIN{system("mkdir "ARGV[1]"_tex");}
 	NR%6==1{x=ARGV[1]"_tex/"++i".tex"; print "\\subj{}\n\\mult" > x; print > x}
-	NR%6==2{print "\\begin{enumerate}[label={\\scshape \\alph*)}]" > x}
+	NR%6==2{print "\\begin{enumerate}[label={\\scshape \\alph*)}\n\t\\setcounter{enumii}{22}]" > x}
 	(NR%6!=1)&&(NR%6!=0){print "\t\\ii",$0 > x}
 	NR%6==0{print "\\end{enumerate}\n\\ans{",$0,"}" > x;close(x)}' $1
